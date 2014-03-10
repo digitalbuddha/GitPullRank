417addc50b7f05e29538e45ee2a94002bd8a2b70
miken <miken@partyrentalltd.com>
2014-03-10 10:51:20 -0400

    changed return set to be min of numberToReturn and the actual number of repo records that exist.

--- src/main/java/com/digitalbuddha/rank/service/RestService.java
+++ src/main/java/com/digitalbuddha/rank/service/RestService.java
@@ -7,6 +7,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import java.util.Arrays;
 
+import static java.lang.Math.min;
+
 
 @Component
 public class RestService {
@@ -15,10 +17,10 @@ public class RestService {
     Logger logger=Logger.getLogger(RestService.class);
 
 
-    public Repository[] findTop5ReposByPullCount(String orgName, int numberToReturn, Boolean expanded) {
+    public Repository[] findTop5ReposByPullCount(String orgName, int numberToReturn, Boolean isExpanded) {
         logger.debug("Requesting All Repos for " + orgName);
         Repository[] allRepos=github.allReposForOrg(orgName);
-        allRepos=addPullData(allRepos,expanded);
+        allRepos=addPullData(allRepos,isExpanded);
         for (int i = 0; i <  allRepos.length-1; i++)
         {
             int index = i;
@@ -30,17 +32,17 @@ public class RestService {
             allRepos[i] = mostPulls;
         }
 
-        return Arrays.copyOf(allRepos,numberToReturn);
+        return Arrays.copyOf(allRepos,min(numberToReturn,allRepos.length));
     }
 
-    private Repository[] addPullData(Repository[] allRepos,  Boolean expanded) {
+    private Repository[] addPullData(Repository[] allRepos,  Boolean isExpanded) {
         for(Repository repo:allRepos)
         { //Remove last parameter as we want all pull records not just 1.
             String pullURL=repo.getPulls_url().replace("{/number}","");
             logger.debug("Requesting " + pullURL);
             Pull[] pulls = github.allPullsForRepo(pullURL);
             repo.setPullCount(pulls.length);
-            if (expanded)
+            if (isExpanded)
             {
                 repo.setPulls(pulls);
             }
